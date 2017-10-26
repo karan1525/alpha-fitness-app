9997fdfe2c56b3dbecb13c6bdea813904eb5db3c
karan1525 <karan1525@yahoo.co.in>
2017-10-25 22:23:24 -0700

    Added main UI fragments and checking something else

--- app/src/main/java/io/github/karan/alphafitness/Fitness_HomeScreen.java
+++ app/src/main/java/io/github/karan/alphafitness/Fitness_HomeScreen.java
@@ -1,8 +1,11 @@
 package io.github.karan.alphafitness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Fitness_HomeScreen extends AppCompatActivity {
@@ -14,5 +17,19 @@ public class Fitness_HomeScreen extends AppCompatActivity {
 
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+
+        checkSomething();
+    }
+
+    public void checkSomething() {
+        Button submitButton = findViewById(R.id.start_workout_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
