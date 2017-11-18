7b237a1976105bbd559535f1a8a10819003644c0
karan1525 <karan1525@yahoo.co.in>
2017-11-18 00:13:49 -0800

    Sample graph added to UI

--- app/src/main/java/io/github/karan/alphafitness/view/Fitness_DetailsScreen.java
+++ app/src/main/java/io/github/karan/alphafitness/view/Fitness_DetailsScreen.java
@@ -10,18 +10,15 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.google.android.gms.maps.model.LatLng;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import io.github.karan.alphafitness.R;
 
 public class Fitness_DetailsScreen extends FragmentActivity {
-
-    ArrayList<LatLng> latLngs = new ArrayList<LatLng>();
-    private double latitude = 37.484532;
-    private double longitude = -122.147448;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,30 +34,53 @@ public class Fitness_DetailsScreen extends FragmentActivity {
         }
 
         LineChart chart = findViewById(R.id.chart);
-        //test data
-        latLngs.add(new LatLng(latitude, longitude));
-        latLngs.add(new LatLng(latitude + 1, longitude + 1));
-        latLngs.add(new LatLng(latitude + 2, longitude + 2));
-        latLngs.add(new LatLng(latitude + 3, longitude + 3));
-        latLngs.add(new LatLng(latitude + 4, longitude + 4));
-        latLngs.add(new LatLng(latitude + 5, longitude + 5));
-        latLngs.add(new LatLng(latitude + 6, longitude + 6));
-        latLngs.add(new LatLng(latitude + 7, longitude + 7));
-        latLngs.add(new LatLng(latitude + 8, longitude + 8));
+        int count = 45;
+        int range = 100;
 
-        List<Entry> entries = new ArrayList<Entry>();
+        ArrayList<Entry> yVals = new ArrayList<>();
 
-        for (LatLng data : latLngs) {
-            // turn your data into Entry objects
-            entries.add(new Entry( (float) data.latitude, (float) data.longitude));
+        for (int i = 0; i < count; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 20;// + (float) // ((mult * // 0.1) / 10);
+            yVals.add(new Entry(i, val));
         }
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-        dataSet.setColor(Color.rgb(255, 150, 101));
-        dataSet.setValueTextColor(Color.rgb(255,255,255));
+        LineDataSet dataSet = new LineDataSet(yVals, "Label"); // add entries to databases
+
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setCubicIntensity(0.2f);
+        //dataSet.setDrawFilled(true);
+        dataSet.setDrawCircles(false);
+        dataSet.setLineWidth(1.8f);
+        dataSet.setCircleRadius(4f);
+        dataSet.setCircleColor(Color.BLACK);
+        dataSet.setHighLightColor(Color.rgb(244, 117, 117));
+        dataSet.setColor(Color.BLACK);
+        dataSet.setFillColor(Color.BLACK);
+        dataSet.setFillAlpha(100);
+        dataSet.setDrawHorizontalHighlightIndicator(false);
+        dataSet.setFillFormatter(new IFillFormatter() {
+            @Override
+            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                return -10;
+            }
+        });
+
+//        dataSet.setColor(Color.rgb(255, 150, 101));
+//        dataSet.setValueTextColor(Color.rgb(255,255,255));
 
         LineData lineData = new LineData(dataSet);
+        lineData.setValueTextSize(9f);
+        lineData.setDrawValues(false);
+
         chart.setData(lineData);
+        chart.getDescription().setEnabled(false);
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.getLegend().setEnabled(false);
+        chart.animateXY(2000, 2000);
+
         chart.invalidate();
     }
 }
